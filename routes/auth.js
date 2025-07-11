391dcaddc822b59c167350564f16bd2ab2c30e5c
limboy95 <limboy_95@hotmail.com>
2025-07-11 09:45:05 +0200

    Fix nodemailer createTransport method name

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const { db } = require('../config/database');
 const router = express.Router();
 
 // Email transporter setup
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   secure: false,
